b662d8b2c252506c1fc2ce34234d25d04d1985cb
TheKoren <korenzol.007@gmail.com>
2023-10-17 15:52:35 +0200

    Hero component now has routing

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Carousel from 'react-material-ui-carousel';
 import {Paper} from '@mui/material';
+import {Link} from 'react-router-dom';
 
 import image1 from '../../assets/info.jpg';
 import image2 from '../../assets/topology.jpg';
@@ -10,14 +11,17 @@ const images = [
     {
         src: image1,
         caption: 'Information',
+        route: '/',
     },
     {
         src: image2,
         caption: "Topology",
+        route: '/topology',
     },
     {
         src: image3,
-        caption: "Analysis"
+        caption: "Analysis",
+        route: '/analysis',
     },
 ];
 
@@ -41,9 +45,9 @@ const Hero = () => {
                                     <div className="poster">
                                         <img src={image.src} alt={`Slide ${index + 1}`}/>
                                     </div>
-                                    <div className="title">
+                                    <Link to={image.route} className = "title">
                                         <h2>{image.caption}</h2>
-                                    </div>
+                                    </Link>
                                 </div>
                             </div>
                         </div>
